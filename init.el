6979c05acf2307d8478fdfcda91a960bc0a645e3
Mikael Wilhelmsson <micke@axevalla.fhsk.se>
2016-08-14 13:34:22 +0200

    initial config

---
+++ init.el
@@ -0,0 +1,16 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(setq package-enable-at-startup nil)
+
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file "~/.emacs.d/myinit.org")
