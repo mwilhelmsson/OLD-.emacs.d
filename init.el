d352e0e63970574679e93ae9e41f4fb2cc831214
Mikael Wilhelmsson <micke@axevalla.fhsk.se>
2016-09-05 08:38:03 +0200

    va?

--- init.el
+++ init.el
@@ -18,4 +18,25 @@
   (require 'org))
 
 (org-babel-load-file "~/.emacs.d/myinit.org")
+<<<<<<< 13e524407724888aa749aa0ee738e027581656b5
 
+=======
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(send-mail-function (quote smtpmail-send-it))
+ '(smtpmail-smtp-server "mail.datahaxx.se")
+ '(smtpmail-smtp-service 587))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(mode-line ((t (:foreground "#000000" :background "#dddddd" :box nil))))
+ '(mode-line-buffer-id ((t (:foreground "#000000" :bold t))))
+ '(mode-line-inactive ((t (:foreground "#000000" :background "#bbbbbb" :box nil))))
+ '(which-func ((t (:foreground "#77aaff")))))
+>>>>>>> va?
