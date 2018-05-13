35d8f5846e3f7676f1f56cba2ce1e6f57e20eb98
Micke Wilhelmsson <micke@nevyn.se>
2018-05-13 21:52:02 +0200

    fixed org-plus-contrib

--- init.el
+++ init.el
@@ -15,13 +15,29 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-(use-package org-plus-contrib
-  :ensure t
-  :config
-  (require 'org))
+(use-package org
+  :ensure org-plus-contrib)
 
 (org-babel-load-file "~/.emacs.d/myinit.org")
  (setq send-mail-function (quote smtpmail-send-it))
  (setq smtpmail-smtp-server "mail.datahaxx.se")
  (setq smtpmail-smtp-service 587)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (web-mode powerline counsel-projectile projectile magit paredit which-key use-package try ox-twbs ox-reveal org-plus-contrib org-bullets moe-theme jedi htmlize flycheck elpy counsel ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(mode-line ((t (:foreground "#000000" :background "#dddddd" :box nil))))
+ '(mode-line-buffer-id ((t (:foreground "#000000" :bold t))))
+ '(mode-line-inactive ((t (:foreground "#000000" :background "#bbbbbb" :box nil))))
+ '(which-func ((t (:foreground "#77aaff")))))
